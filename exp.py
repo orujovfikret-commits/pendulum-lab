06dd39d39a0a11fba96e63f9abcb1894dbc61e08
orujovfikret-commits <orujov.fikret@gmail.com>
2026-09-06 20:05:05 +0400

    Simplify exp.py

--- exp.py
+++ exp.py
@@ -1,18 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load available dataset
-df = pd.read_csv('L0.21_run1.csv')
+df = pd.read_csv('L0.50_run1.csv')
 
-plt.figure(figsize=(10, 5))
-plt.plot(df['Time (s)'], df['Gyroscope x (rad/s)'], label='Gyroscope x', alpha=0.7)
-plt.plot(df['Time (s)'], df['Gyroscope y (rad/s)'], label='Gyroscope y', alpha=0.7)
-plt.plot(df['Time (s)'], df['Gyroscope z (rad/s)'], label='Gyroscope z', linewidth=1.5)
+plt.plot(df['Time (s)'], df['Gyroscope x (rad/s)'], label='x')
+plt.plot(df['Time (s)'], df['Gyroscope y (rad/s)'], label='y')
+plt.plot(df['Time (s)'], df['Gyroscope z (rad/s)'], label='z')
 
 plt.xlabel('Time (s)')
-plt.ylabel('Angular Velocity (rad/s)')
-plt.title('3-Axis Gyroscope Motion Data')
+plt.ylabel('Gyroscope (rad/s)')
+plt.title('Gyroscope Data')
 plt.legend()
-plt.grid(True)
-plt.tight_layout()
 plt.show()
